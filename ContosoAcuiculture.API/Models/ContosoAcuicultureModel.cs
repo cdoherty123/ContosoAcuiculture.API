7a98f173927863e0038ec123890c713ecc08aebc
Courtney Doherty <cdoherty@microsoft.com>
2021-12-06 22:04:46 +0000

    done empty api moving forward with cosmos db interface

--- ContosoAcuiculture.API/Models/ContosoAcuicultureModel.cs
+++ ContosoAcuiculture.API/Models/ContosoAcuicultureModel.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
 
 namespace ContosoAcuiculture.API.Models
 
@@ -15,11 +16,19 @@ namespace ContosoAcuiculture.API.Models
     {
         //These are only samples until we recieve the database
             [Key]
+            [JsonProperty(PropertyName = "id")]
             public int ID { get; set; }
+            [JsonProperty(PropertyName = "shrimpid")]
             public int ShrimpID { get; set; }
+            [JsonProperty(PropertyName = "shrimpcolor")]
+            public string ShrimpColor{ get; set; }
+            [JsonProperty(PropertyName = "averageweight")]
             public float AverageWeight { get; set; }
+            [JsonProperty(PropertyName = "dailyfeed")]
             public int DailyFeed { get; set; }
-            public int EstimatedSurvival { get; set; } 
+            [JsonProperty(PropertyName = "estimatedsurvival")]
+            public int EstimatedSurvival { get; set; }
+            [JsonProperty(PropertyName = "observations")]
             public string Observations { get; set; }
         
     }
